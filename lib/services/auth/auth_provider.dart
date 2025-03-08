40af6dece83f481b1e4961ae0ce67cff2f9181e1
Galuh <galuh.garhadi408@gmail.com>
2025-03-09 00:52:15 +0700

    step-10

--- lib/services/auth/auth_provider.dart
+++ lib/services/auth/auth_provider.dart
@@ -3,7 +3,7 @@ import 'package:prakmb/services/auth/auth_user.dart';
 abstract class AuthProvider {
   Future<void> initialize();
   AuthUser? get currentUser;
-  Future<AuthUser> login({
+  Future<AuthUser> logIn({
     required String email,
     required String password,
   });
