436c6ac20251eade7916e8939fb4403824c1e38a
Galuh <galuh.garhadi408@gmail.com>
2025-03-12 23:04:40 +0700

    step-16

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:prakmb/constants/routes.dart';
 import 'package:prakmb/services/auth/auth_service.dart';
 import 'package:prakmb/views/login_view.dart';
-import 'package:prakmb/views/notes/new_note_view.dart';
+import 'package:prakmb/views/notes/create_update_note_view.dart';
 import 'package:prakmb/views/notes/notes_view.dart';
 import 'package:prakmb/views/register_view.dart';
 import 'package:prakmb/views/verify_email_view.dart';
@@ -21,7 +21,7 @@ void main() {
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
         verifyEmailRoute: (context) => const VerifyEmailView(),
-        newNoteRoute: (context) => const NewNoteView(),
+        createOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
       },
     ),
   );
