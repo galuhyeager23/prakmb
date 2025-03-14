5fbbbbed421d0e32a68c8fe3fcf149d8e91a852d
Galuh <galuh.garhadi408@gmail.com>
2025-03-14 20:45:29 +0700

    step-20

--- lib/views/notes/notes_list_view.dart
+++ lib/views/notes/notes_list_view.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:prakmb/services/crud/notes_service.dart';
+import 'package:prakmb/services/cloud/cloud_note.dart';
 import 'package:prakmb/utilities/dialogs/delete_dialog.dart';
 
-typedef NoteCallback = void Function(DatabaseNote note);
+typedef NoteCallback = void Function(CloudNote note);
 
 class NotesListView extends StatelessWidget {
-  final List<DatabaseNote> notes;
+  final Iterable<CloudNote> notes;
   final NoteCallback onDeleteNote;
   final NoteCallback onTap;
 
   const NotesListView({
-    super.key,
+    Key? key,
     required this.notes,
     required this.onDeleteNote,
     required this.onTap,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: notes.length,
       itemBuilder: (context, index) {
-        final note = notes[index];
+        final note = notes.elementAt(index);
         return ListTile(
           onTap: () {
             onTap(note);
